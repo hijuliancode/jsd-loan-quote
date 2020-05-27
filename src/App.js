6f616d976f88f7d4b32ea2a80485607ba95c462c
JulianCode <hijuliansosa@protonmail.com>
2020-05-27 04:49:59 -0500

    Creating a Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from './components/header';
+import Form from './components/form';
 
 function App() {
   return (
     <Fragment>
       <Header title="Cotizador de prestamos" />
+      <div className="container">
+        <Form />
+      </div>
     </Fragment>
   );
 }
