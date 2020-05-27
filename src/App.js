6d14e6d5f5e9acf5c4a3894797a79a5a36eed2b0
JulianCode <hijuliansosa@protonmail.com>
2020-05-27 04:20:46 -0500

    Starting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <Fragment>
+      <Header />
+      <Header />
+      <Header />
+    </Fragment>
   );
 }
 
